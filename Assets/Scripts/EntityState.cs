b43c7d96b739e59bcdedc362f5c79d9e14d3ee28
codewithminer <saeed60914376@gmail.com>
2025-10-01 17:51:25 +0200

    idle/move anim step 1

--- Assets/Scripts/EntityState.cs
+++ Assets/Scripts/EntityState.cs
@@ -9,30 +9,34 @@ public abstract class EntityState
 
     protected Player player;
     protected StateMachine stateMachine;
-    protected string stateName;
+    protected string animBoolName;
+
+    Animator anim;
 
     public EntityState(Player player, StateMachine stateMachine, string stateName)
     {
         this.player = player;
         this.stateMachine = stateMachine;
-        this.stateName = stateName;
+        this.animBoolName = stateName;
+
+        anim = player.anim;
     }
 
     public void Enter()
     {
         // everytime state will be changed, Enter will be called
-        Debug.Log("I enter " + stateName);
+        anim.SetBool(animBoolName, true);
     }
 
     public virtual void Update()
     {
         // we going to run logic of the state here
-        Debug.Log("I update " + stateName);
+        Debug.Log("I update " + animBoolName);
     }
 
     public virtual void Exit()
     {
         // Exit will be called, everytime we exit state and change to a new one
-        Debug.Log("I exit " + stateName);
+        anim.SetBool(animBoolName, false);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Animator anim {get; private set; }
+    
     private PlayerInputSet input;
     private StateMachine stateMachine;
 
@@ -12,6 +14,8 @@ public class Player : MonoBehaviour
 
     void Awake()
     {
+        anim = GetComponentInChildren<Animator>();
+
         input = new PlayerInputSet();
         stateMachine = new StateMachine();
 

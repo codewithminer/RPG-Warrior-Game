33b4d38deebda4aea7451f8056a03957c3902e86
codewithminer <saeed60914376@gmail.com>
2025-10-13 20:12:14 +0200

    endless background

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,23 +4,38 @@ public class ParallaxBackground : MonoBehaviour
 {
     private Camera mainCamera;
     private float lastCameraPositionX;
+    private float cameraHalfWidth;
 
     [SerializeField] private ParallaxLayer[] backgroundLayers;
 
-    void Awake()
+    private void Awake()
     {
         mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        CalculateImageLength();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
         float currentCameraPositionX = mainCamera.transform.position.x;
         float distanceToMove = currentCameraPositionX - lastCameraPositionX;
         lastCameraPositionX = currentCameraPositionX;
 
-        foreach(ParallaxLayer layer in backgroundLayers)
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;
+
+        foreach (ParallaxLayer layer in backgroundLayers)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+    
+    private void CalculateImageLength()
+    {
+        foreach(ParallaxLayer layer in backgroundLayers)
+        {
+            layer.CalculateImageWidth();
         }
     }
 }

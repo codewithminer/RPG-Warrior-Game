aa7112abe64b4229c7417b8161bd07d8f9b93ff2
codewithminer <saeed60914376@gmail.com>
2026-01-02 18:29:33 +0100

    itemToolTip

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class UI : MonoBehaviour
 {
     public UI_SkillToolTip skillToolTip;
+    public UI_ItemToolTip itemToolTip;
+
     public UI_SkillTree skillTree;
     private bool skillTreeEnabled;
 
     private void Awake()
     {
         skillToolTip = GetComponentInChildren<UI_SkillToolTip>();
+        itemToolTip = GetComponentInChildren<UI_ItemToolTip>();
         skillTree = GetComponentInChildren<UI_SkillTree>(true); // (true) makes sure to get component even if it's inactive
     }
 
--- Assets/Scripts/UI/UI_ItemSlot.cs
+++ Assets/Scripts/UI/UI_ItemSlot.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UI_ItemSlot : MonoBehaviour, IPointerDownHandler
+public class UI_ItemSlot : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public Inventory_Item itemInSlot {get; private set;}
     protected Inventory_Player inventory;
+    protected UI ui;
+    protected RectTransform rect;
 
     [Header("UI Slot Setup")]
     [SerializeField] private Image itemIcon;
@@ -14,6 +16,8 @@ public class UI_ItemSlot : MonoBehaviour, IPointerDownHandler
 
     protected void Awake()
     {
+        ui = GetComponentInParent<UI>();
+        rect = GetComponent<RectTransform>();
         inventory = FindAnyObjectByType<Inventory_Player>();
     }
 
@@ -36,9 +40,23 @@ public class UI_ItemSlot : MonoBehaviour, IPointerDownHandler
 
     public virtual void OnPointerDown(PointerEventData eventData)
     {
-        if (itemInSlot == null)
+        if (itemInSlot == null || itemInSlot.itemData.itemType == ItemType.Material)
             return;
+
         inventory.TryEquipItem(itemInSlot);
-        
+        if (itemInSlot == null)
+            ui.itemToolTip.ShowToolTip(false, null);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (itemInSlot == null) return;
+
+        ui.itemToolTip.ShowToolTip(true, rect, itemInSlot);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ui.itemToolTip.ShowToolTip(false, null);
     }
 }
